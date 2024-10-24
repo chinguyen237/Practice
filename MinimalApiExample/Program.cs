db0477ebcbe4847202551b85b8e63987fa2f520a
HARVEYNASH\chinguyentl <chinguyentl@nashtechglobal.com>
2024-10-24 14:45:32 +0700

    first comment

---
+++ MinimalApiExample/Program.cs
@@ -0,0 +1,23 @@
+// Challenge 1:
+// - Create a new .Net Core minimal API
+// - Create a new class model Category (ID, Name)
+// - Create one endpoint to get category data (hard-coded data)
+// - Run and test endpoint with postman
+
+using MinimalApiExample.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/categories", () =>
+{
+var categories = new List<Category>
+{
+new Category { ID = 1, Name = "Electronics" },
+new Category { ID = 2, Name = "Books" },
+new Category { ID = 3, Name = "Clothing" }
+};
+return categories;
+});
+
+app.Run();
